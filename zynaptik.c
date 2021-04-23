ff56ca26edb27ada461936ce235d6d5945765e3f
jofemodo <fernando@zynthian.org>
2021-04-23 14:04:46 +0200

    Implement partially analog output.

--- zynaptik.c
+++ zynaptik.c
@@ -32,12 +32,14 @@
 #include <time.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <stdbool.h> 
 
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
 #include <mcp23017.h>
 #include <mcp23x0817.h>
 #include <ads1115.h>
+#include <MCP4728.h>
 
 #include "zyncoder.h"
 
@@ -73,16 +75,27 @@ void set_ads1115_gain(uint16_t base_pin, uint8_t gain) {
 	digitalWrite(base_pin, gain);
 }
 
+
 //-----------------------------------------------------------------------------
-// CV-IN: Generate MIDI CC from Analog Inputs
+// MCP4728 Stuff
+//-----------------------------------------------------------------------------
+
+void * mcp4728_chip;
+
+void init_mcp4728(uint16_t i2c_address) {
+	mcp4728_chip = mcp4728_initialize(2, 3, -1, i2c_address);
+}
+
+//-----------------------------------------------------------------------------
+// CV-IN: Generate MIDI from Analog Inputs: CC, Pitchbend, Channel Pressure
 //-----------------------------------------------------------------------------
 
 void setup_zynaptik_cvin(uint8_t i, uint8_t midi_evt, uint8_t midi_chan, uint8_t midi_num) {
 	zyncvins[i].pin = ZYNAPTIK_ADS1115_BASE_PIN + i;
 	zyncvins[i].val = 0;
-	zyncvins[i].midi_evt = midi_evt;
-	zyncvins[i].midi_chan = midi_chan;
-	zyncvins[i].midi_num = midi_num;
+	zyncvins[i].midi_evt = midi_evt & 0xF;
+	zyncvins[i].midi_chan = midi_chan & 0xF;
+	zyncvins[i].midi_num = midi_num & 0xF7;
 	zyncvins[i].midi_val = 0;
 	zyncvins[i].enabled = 1;
 }
@@ -91,7 +104,7 @@ void disable_zynaptik_cvin(uint8_t i) {
 	zyncvins[i].enabled = 0;
 }
 
-void send_zynaptik_cvin_midi(uint8_t i) {
+void zynaptik_cvin_to_midi(uint8_t i) {
 	if (zyncvins[i].midi_evt==PITCH_BENDING) {
 		//Send MIDI event to engines and ouput (ZMOPS)
 		internal_send_pitchbend_change(zyncvins[i].midi_chan, zyncvins[i].val);
@@ -121,7 +134,7 @@ void * poll_zynaptik_cvins(void *arg) {
 		for (i=0;i<MAX_NUM_ZYNCVINS;i++) {
 			if (zyncvins[i].enabled) {
 				zyncvins[i].val = analogRead(zyncvins[i].pin);
-				send_zynaptik_cvin_midi(i);
+				zynaptik_cvin_to_midi(i);
 				//printf("ZYNAPTIK CV-IN [%d] => %d\n", i, zyncvins[i].val);
 			}
 		}
@@ -142,6 +155,96 @@ pthread_t init_poll_zynaptik_cvins() {
 	}
 }
 
+
+//-----------------------------------------------------------------------------
+// CV-OUT: Set Analog Outputs from MIDI: CC, Pitchbend, Channel Pressure, Notes (velocity+pitchbend)
+//-----------------------------------------------------------------------------
+
+void setup_zynaptik_cvout(uint8_t i, uint8_t midi_evt, uint8_t midi_chan, uint8_t midi_num) {
+	zyncvouts[i].midi_val = 0;
+
+	zyncvouts[i].midi_evt = midi_evt & 0xF;
+	zyncvouts[i].midi_chan = midi_chan & 0xF;
+	zyncvouts[i].midi_num = midi_num & 0xF7;
+
+	if (midi_evt==PITCH_BENDING || midi_evt==CHAN_PRESS) {
+		zyncvouts[i].midi_event_mask=0xFF00;
+		zyncvouts[i].midi_event_temp=((midi_evt&0xF)<<12) | ((midi_chan&0xF)<<8);
+	}
+	else if (midi_evt==CTRL_CHANGE) {
+		zyncvouts[i].midi_event_mask=0xFFF0;
+		zyncvouts[i].midi_event_temp=((midi_evt&0xF)<<12) | ((midi_chan&0xF)<<8) | (midi_num&0xF7);
+	}
+
+	zyncvouts[i].enabled = 1;
+}
+
+void disable_zynaptik_cvout(uint8_t i) {
+	zyncvouts[i].enabled = 0;
+}
+
+void zynaptik_midi_to_cvout(jack_midi_event_t *ev) {
+	uint8_t event_type= ev->buffer[0] >> 4;
+
+	for (int i=0;i<MAX_NUM_ZYNCVOUTS;i++) {
+		if  (!zyncvouts[i].enabled) continue;
+
+		if (event_type==
+	}
+	
+	if (zyncvouts[i].midi_evt==PITCH_BENDING) {
+		//Send MIDI event to engines and ouput (ZMOPS)
+		internal_send_pitchbend_change(zyncvins[i].midi_chan, zyncvins[i].val);
+	} else {
+		uint8_t mv = zyncvins[i].val>>8;
+		if (mv!=zyncvins[i].midi_val) {
+			//printf("ZYNAPTIK CV-IN [%d] => MIDI %d\n", i, mv);
+			zyncvins[i].midi_val = mv;
+			if (zyncvins[i].midi_evt==CTRL_CHANGE) {
+				//Send MIDI event to engines and ouput (ZMOPS)
+				internal_send_ccontrol_change(zyncvins[i].midi_chan, zyncvins[i].midi_num, mv);
+				//Update zyncoders
+				midi_event_zyncoders(zyncvins[i].midi_chan, zyncvins[i].midi_num, mv);
+				//Send MIDI event to UI
+				write_zynmidi_ccontrol_change(zyncvins[i].midi_chan, zyncvins[i].midi_num, mv);
+			} else if (zyncvins[i].midi_evt==CHAN_PRESS) {
+				//Send MIDI event to engines and ouput (ZMOPS)
+				internal_send_chan_press(zyncvins[i].midi_chan, mv);
+			} 
+		}
+	}
+}
+
+void * refresh_zynaptik_cvouts(void *arg) {
+	int i;
+	float buffer[MAX_NUM_ZYNCVOUTS];
+	while (1) {
+		for (i=0;i<MAX_NUM_ZYNCVOUTS;i++) {
+			if (zyncvouts[i].enabled) {
+				buffer[i] = 5.0*zyncvouts[i].midi_val/127.0;
+			} else {
+				buffer[i] = 0;
+			}
+			mcp4728_multipleinternal(mcp4728_chip, buffer, 0);
+			//printf("ZYNAPTIK CV-OUT => [%f, %f, %f, %f]\n", buffer[0], buffer[1], buffer[2], buffer[3]);
+		}
+		usleep(REFRESH_ZYNAPTIK_CVOUTS_US);
+	}
+	return NULL;
+}
+
+pthread_t init_refresh_zynaptik_cvouts() {
+	pthread_t tid;
+	int err=pthread_create(&tid, NULL, &refresh_zynaptik_cvouts, NULL);
+	if (err != 0) {
+		printf("Zyncoder: Can't create zynaptik CV-OUT refresh thread :[%s]", strerror(err));
+		return 0;
+	} else {
+		printf("Zyncoder: Zynaptik CV-OUT refresh thread created successfully\n");
+		return tid;
+	}
+}
+
 //-----------------------------------------------------------------------------
 // Zynaptik Library Initialization
 //-----------------------------------------------------------------------------
@@ -151,16 +254,26 @@ int init_zynaptik() {
 	for (i=0;i<MAX_NUM_ZYNCVINS;i++) {
 		zyncvins[i].enabled=0;
 	}
+	for (i=0;i<MAX_NUM_ZYNCVOUTS;i++) {
+		zyncvouts[i].enabled=0;
+	}
+
+	mcp4728_chip = NULL;
 
 	if (strstr(ZYNAPTIK_CONFIG, "16xSWITCH")) {
 		zynaptik_mcp23017_node = init_mcp23017(ZYNAPTIK_MCP23017_BASE_PIN, ZYNAPTIK_MCP23017_I2C_ADDRESS, ZYNAPTIK_MCP23017_INTA_PIN, ZYNAPTIK_MCP23017_INTB_PIN, zynaptik_mcp23017_bank_ISRs);
 	}
-
 	if (strstr(ZYNAPTIK_CONFIG, "4xAD")) {
 		init_ads1115(ZYNAPTIK_ADS1115_BASE_PIN, ZYNAPTIK_ADS1115_I2C_ADDRESS);
 		init_poll_zynaptik_cvins();
 	}
+	//TODO Implement in webconf!!!
+	if (strstr(ZYNAPTIK_CONFIG, "4xDA") || 1) {
+		init_mcp4728(ZYNAPTIK_MCP4728_I2C_ADDRESS);
+		init_refresh_zynaptik_cvouts();
+	}
 
+	
 	return 1;
 }
 
--- zynaptik.h
+++ zynaptik.h
@@ -65,10 +65,21 @@ void init_ads1115(uint16_t base_pin, uint16_t i2c_address);
 void set_ads1115_gain(uint16_t base_pin, uint8_t gain);
 
 //-----------------------------------------------------------------------------
-// CV-IN: Generate MIDI CC from Analog Inputs
+// MCP4728 Stuff
 //-----------------------------------------------------------------------------
 
-#define MAX_NUM_ZYNCVINS 8
+//Default config for Zynaptik's MCP4728
+#if !defined(ZYNAPTIK_MCP4728_I2C_ADDRESS)
+	#define ZYNAPTIK_MCP4728_I2C_ADDRESS 0x60
+#endif
+
+void init_mcp4728(uint16_t i2c_address);
+
+//-----------------------------------------------------------------------------
+// CV-IN: Generate MIDI from Analog Inputs: CC, Pitchbend, Channel Pressure
+//-----------------------------------------------------------------------------
+
+#define MAX_NUM_ZYNCVINS 4
 
 struct zyncvin_st {
 	uint8_t enabled;
@@ -84,12 +95,47 @@ struct zyncvin_st zyncvins[MAX_NUM_ZYNCVINS];
 
 void setup_zynaptik_cvin(uint8_t i, uint8_t midi_evt, uint8_t midi_chan, uint8_t midi_num);
 void disable_zynaptik_cvin(uint8_t i);
-void send_zynaptik_cvin_midi(uint8_t i);
+void zynaptik_cvin_to_midi(uint8_t i);
 
 //CV-IN Polling interval
 #define POLL_ZYNAPTIK_CVINS_US 40000
 
 pthread_t init_poll_zynaptik_cvins();
+
+//-----------------------------------------------------------------------------
+// CV-OUT: Set Analog Outputs from MIDI: CC, Notes (velocity+pitchbend)
+//-----------------------------------------------------------------------------
+
+#define MAX_NUM_ZYNCVOUTS 4
+
+struct zyncvout_st {
+	uint8_t enabled;
+
+	uint8_t midi_val;
+
+	uint8_t midi_evt;
+	uint8_t midi_chan;
+	uint8_t midi_num;
+
+	uint16_t midi_event_temp;
+	uint16_t midi_event_mask;
+};
+struct zyncvout_st zyncvouts[MAX_NUM_ZYNCVOUTS];
+
+void setup_zynaptik_cvout(uint8_t i, uint8_t midi_evt, uint8_t midi_chan, uint8_t midi_num);
+void disable_zynaptik_cvout(uint8_t i);
+void zynaptik_midi_to_cvout(jack_midi_event_t *ev);
+
+//CV-OUT Refresh interval
+#define REFRESH_ZYNAPTIK_CVOUTS_US 40000
+
+pthread_t init_refresh_zynaptik_cvouts();
+
+//-----------------------------------------------------------------------------
+// MIDI processing
+//-----------------------------------------------------------------------------
+
+void midi_event_zynaptik(uint32_t ev);
 
 //-----------------------------------------------------------------------------
 // Zynaptik Library Initialization
