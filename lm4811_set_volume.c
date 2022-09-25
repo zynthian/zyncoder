351e0259f4a10834c00ff0ad46b0f556a9177725
jofemodo <fernando@zynthian.org>
2022-09-25 16:42:05 +0200

    Add program to set LM4811 volume (headphone amplifier on Z2).

---
+++ lm4811_set_volume.c
@@ -0,0 +1,45 @@
+/*
+ * ******************************************************************
+ * ZYNTHIAN PROJECT: Headphones Volume Control for LM4811 amplifier
+ * 
+ * Basic CLI for setting volume
+ * Usage:
+ *   lm4811_set_volume [0-15]
+ *
+ * Copyright (C) 2015-2021 Fernando Moyano <jofemodo@zynthian.org>
+ *
+ * ******************************************************************
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * For a full copy of the GNU General Public License see the LICENSE.txt file.
+ * 
+ * ******************************************************************
+ */
+
+#include <wiringPi.h>
+#include "lm4811.h"
+
+//-------------------------------------------------------------------
+
+int main(int argc, char *argv[]) {
+	uint8_t vol = 10;
+	if (argc>=2) {
+		vol = atoi(argv[1]);
+	}
+	wiringPiSetup();
+	lm4811_init();
+	printf("Setting LM4811 volume to %d\n", vol);
+	lm4811_set_volume(vol);
+	//lm4811_end();
+}
+
+//-------------------------------------------------------------------
